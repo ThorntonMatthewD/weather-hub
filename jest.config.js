7f0b8e987d0a89ff07990b85b3281e18e295e23a
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-02-18 21:34:48 -0500

    Fix jest config

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  setupFilesAfterEnv: [
+    "@testing-library/jest-dom/extend-expect"
+  ],
+  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+};
