5e68b5172a301f799165f54a354955744ce66585
Matthew Thornton <44626690+ThorntonMatthewD@users.noreply.github.com>
2023-02-18 21:52:06 -0500

    More jest config fixes

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,14 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'node',
+  // I had to install jest-environment-jsdom for this to work
+  testEnvironment: 'jsdom',
   setupFilesAfterEnv: [
     "@testing-library/jest-dom/extend-expect"
   ],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node", "css"],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sss|styl)$': '<rootDir>/node_modules/jest-css-modules'
+  }
 };
